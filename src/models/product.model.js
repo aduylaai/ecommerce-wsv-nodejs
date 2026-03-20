4a902e1c75f3279693bba38bef43f9c1b91ddbda
aduylaai <aduylaai@gmail.com>
2026-03-20 09:40:30 +0700

    Add product schema

---
+++ src/models/product.model.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const {model, Schema} = require('mongoose')
+
+const DOCUMENT_NAME  = 'Product'
+const COLLECTION_NAME = 'Products'
+
+const productSchema = new Schema({
+  product_name: { type: String, required: true },
+  product_thumb: { type: String, required: true },
+  product_description: String,
+  product_price: { type: Number, required: true },
+  product_quantity: { type: Number, required: true },
+  product_type: { type: String, required: true, enum: ['Electronics', 'Clothing', 'Furniture'] },
+  product_shop: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  product_attributes: { type: Schema.Types.Mixed, required: true }
+}, {
+  collection: COLLECTION_NAME,
+  timestamps: true
+})
+
+
+// define the prod types:
+const clothingSchema = new Schema({
+    brand: {
+        type: String,
+        required:true
+    },
+    size:String,
+    material: String
+},{
+    timestamps:true,
+    collection:'Clothes'
+})
+
+
+const electronicSchema = new Schema({
+    manufactor: {
+        type: String,
+        required:true
+    },
+    model:String,
+    color: String
+},{
+    timestamps:true,
+    collection:'Electronics'
+})
+
+
+module.exports = {
+    product: model(DOCUMENT_NAME,productSchema),
+    clothing: model('Clothing', clothingSchema),
+    electronic: model('Electronic', electronicSchema)
+}
